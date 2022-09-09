cdc548e2888ec67e3da0a9dca06d54369c4aee70
Neu3BecTHo <Hosting.By.Neu3BecTHo@gmail.com>
2022-09-09 23:26:37 +0500

    Bugfixes files.
    Fix JS and HTML files.

--- NeOS/JavaScript/app.js
+++ NeOS/JavaScript/app.js
@@ -1,14 +1,14 @@
-let page = document.querySelector('.page');
+/**let page = document.querySelector('.page');
 let theme = document.querySelector('.theme');
 let message = document.querySelector('.message-theme');
 let name = document.querySelector('.name');
 let confirm_name = document.querySelector('.confirm-name');
 
 theme.onclick = function() {
- page.classList.toggle("pink-for-main");
- page.classList.toggle("green-for-main");
+    page.classList.toggle("light-theme");
+    page.classList.toggle("black-theme");
 };
 
 confirm_name.onclick = function() {
 message.textContent = name.value;
-};
+};**/
---
+++ NeOS/JavaScript/theme.js
@@ -0,0 +1,5 @@
+let change_theme = document.querySelector('.theme');
+change_theme.addEventListener('click', function() {
+    document.body.classList.toggle('light-theme');
+    document.body.classList.toggle('black-theme');
+});
