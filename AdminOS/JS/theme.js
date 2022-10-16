69ba7dcb7b825b977500700cb323ce6a2b84030e
Neu3BecTHo <Hosting.By.Neu3BecTHo@gmail.com>
2022-10-16 07:50:58 +0500

    fix

--- AdminOS/JS/theme.js
+++ AdminOS/JS/theme.js
@@ -1,5 +1,5 @@
 let change_theme = document.querySelector('.theme');
 change_theme.addEventListener('click', function() {
-    document.body.classList.toggle('light-theme');
     document.body.classList.toggle('black-theme');
+    document.body.classList.toggle('light-theme');
 });
